7f5f257dc14701165168cb13ed72fb0dd53ea762
Jordan D <jordan.dsouza@ntlworld.com>
2024-07-17 13:26:51 +0100

    BoardCell changes: OnClick listener, style changes

--- client/src/components/BoardCell.js
+++ client/src/components/BoardCell.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function BoardCell(props) {
     return (
-        <div className = "board-cell" id={props.number + 1}>
+        <div className="board-cell" id={props.number + 1} onClick={() => console.log("Bet: " + (props.number + 1))}>
             {props.number + 1}
         </div>
     );
